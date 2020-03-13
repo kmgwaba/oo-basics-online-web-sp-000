8b39392b0324b3a0622a6e9d52f2f1074f2b245d
Kathia Mgwaba <mgwaba68@gmail.com>
2020-03-13 17:59:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition = new 
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
@@ -11,7 +11,7 @@ class Shoe
   end
 
   def cobble
-    puts "Your shoes is as good as new!"
+    puts "Your shoes has been repaired."
 
   end
 
